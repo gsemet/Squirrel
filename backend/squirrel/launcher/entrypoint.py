91a1dc7dcf10247c56aa97a9af78c534f852a8d3
Gaetan Semet <gaetan@xeberon.net>
2015-03-15 03:16:33 +0100

    fixed loggers
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- backend/squirrel/launcher/entrypoint.py
+++ backend/squirrel/launcher/entrypoint.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import logging
 import logging.config as logging_config
 
 from squirrel.common.i18n import setupI18n
@@ -11,9 +12,12 @@ from squirrel.services.db import connectDatabase
 from squirrel.services.plugin_loader import loadPlugins
 from squirrel.services.serve_backend import serveBackend
 
+log = logging.getLogger(__name__)
+
 
 def setupLogger():
     logging_config.fileConfig(Config().frontend.logging_conf_full_path)
+    logging.debug("Logger configured by: {}".format(Config().frontend.logging_conf_full_path))
 
 
 def run():
