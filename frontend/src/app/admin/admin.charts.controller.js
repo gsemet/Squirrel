9c06d22948e57108f336bbed354a4375710616a5
Gaetan Semet <gaetan@xeberon.net>
2015-03-30 00:32:40 +0200

    Trying area gradiant in chart
    highstocks does not seem to support gradiant like in charts.
    Several bug found in the current highstocks provided
    by nghighstocks

--- frontend/src/app/admin/admin.charts.controller.js
+++ frontend/src/app/admin/admin.charts.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('squirrel').controller('AdminChartsCtrl',
 
-  ['$scope',
+  ['$scope', '$http',
 
-    function($scope) {
+    function($scope, $http) {
 
       $scope.chartConfig = {
         options: {
@@ -124,6 +124,75 @@ angular.module('squirrel').controller('AdminChartsCtrl',
 
       //////////////////////////////////////////////////////////////////////////////////////////////
 
+      $http.get("../app/admin/eurusd.json").then(
+        function(result) {
+          console.log("request sent:" + +JSON.stringify(result.data));
+
+          $scope.chartEurUsdConfig = {
+
+            title: {
+              text: 'AAPL stock price by minute'
+            },
+
+            subtitle: {
+              text: 'Using explicit breaks for nights and weekends'
+            },
+
+            xAxis: {
+              breaks: [{ // Nights
+                from: Date.UTC(2011, 9, 6, 16),
+                to: Date.UTC(2011, 9, 7, 8),
+                repeat: 24 * 36e5
+              }, { // Weekends
+                from: Date.UTC(2011, 9, 7, 16),
+                to: Date.UTC(2011, 9, 10, 8),
+                repeat: 7 * 24 * 36e5
+              }]
+            },
+
+            rangeSelector: {
+              buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+              }, {
+                type: 'day',
+                count: 1,
+                text: '1D'
+              }, {
+                type: 'all',
+                count: 1,
+                text: 'All'
+              }],
+              selected: 1,
+              inputEnabled: false
+            },
+
+            series: [{
+              name: 'AAPL',
+              type: 'area',
+              data: result.data,
+              gapSize: 5,
+              tooltip: {
+                valueDecimals: 2
+              },
+              fillColor: {
+                linearGradient: {
+                  x1: 0,
+                  y1: 0,
+                  x2: 0,
+                  y2: 1
+                },
+                stops: [
+                      [0, Highcharts.getOptions().colors[0]],
+                      [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                  ]
+              },
+              threshold: null
+              }]
+          }
+        });
+      //////////////////////////////////////////////////////////////////////////////////////////////
     }
   ]
 );
