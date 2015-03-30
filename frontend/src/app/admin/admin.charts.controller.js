8dd9bdfd6216cd7de5d53ad13988e66a7e920790
Gaetan Semet <gaetan@xeberon.net>
2015-03-30 20:58:02 +0200

    crossfilter seems to work

--- frontend/src/app/admin/admin.charts.controller.js
+++ frontend/src/app/admin/admin.charts.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('squirrel').controller('AdminChartsCtrl',
 
-  ['$scope', '$http', "$q",
+  ['$scope', '$http', "$q", "Crossfilter",
 
-    function($scope, $http, $q) {
+    function($scope, $http, $q, Crossfilter) {
 
       $scope.chartConfig = {
         options: {
@@ -763,7 +763,102 @@ angular.module('squirrel').controller('AdminChartsCtrl',
         });
       }, 100);
 
+      //////////////////////////////////////////////////////////////////////////////////////////////
 
+
+      /**
+       * @property words
+       * @type {Object}
+       */
+      $scope.words = {};
+
+      /**
+       * @property word
+       * @type {String}
+       */
+      $scope.word = '';
+
+      /**
+       * @property loading
+       * @type {Boolean}
+       */
+      $scope.loading = true;
+
+      /**
+       * @property pageNumber
+       * @type {Number}
+       * @default 50
+       */
+      $scope.pageNumber = 50;
+
+      /**
+       * @property countGrouped
+       * @type {Array}
+       */
+      $scope.countGrouped = [];
+
+      /**
+       * @property currentCountFilter
+       * @type {Number}
+       */
+      $scope.currentCountFilter = 0;
+
+      // When the Crossfilter collection has been updated.
+      $scope.$on('crossfilter/updated', function crossfilterUpdated() {
+
+        if (angular.isDefined($scope.words.groupBy)) {
+          $scope.countGrouped = $scope.words.groupBy('wordCount');
+        }
+
+      });
+
+      /**
+       * @method toggleCountFilter
+       * @param count {Number}
+       * @return {void}
+       */
+      $scope.toggleCountFilter = function toggleCountFilter(count) {
+
+        if ($scope.currentCountFilter == count) {
+
+          $scope.currentCountFilter = null;
+          $scope.words.unfilterBy('wordCount');
+          return;
+
+        }
+
+        $scope.currentCountFilter = count;
+        $scope.words.filterBy('wordCount', count);
+
+      };
+
+      // Fetch all of the words to create the Crossfilter from.
+      $http.get('../app/admin/words.json').then(function then(response) {
+
+        // Voila!
+        $scope.words = new Crossfilter(response.data, '$id', 'persistent');
+        $scope.words.addDimension('wordCount', function wordCount(model) {
+          return model.word.length;
+        });
+
+        $scope.countGrouped = $scope.words.groupBy('wordCount');
+        $scope.loading = false;
+
+      });
+
+      /**
+       * @method applyWordFilter
+       * @param word {String}
+       * @param customFilter {Function}
+       * @return {void}
+       */
+      $scope.applyWordFilter = function applyWordFilter(word, customFilter) {
+
+        $scope.pageNumber = 50;
+        $scope.words.filterBy('word', word, customFilter);
+        $scope.word = word;
+
+      };
       //////////////////////////////////////////////////////////////////////////////////////////////
     }
   ]
