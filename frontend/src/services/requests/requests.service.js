593fb088b13a52a94cfd9212672c4aa9cdc9b99b
Gaetan Semet <gaetan@xeberon.net>
2015-04-02 14:56:01 +0200

    environment and request services
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ frontend/src/services/requests/requests.service.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('squirrel').factory('request',
+
+  ['$http', '$q',
+
+    function($http, $q) {
+
+      // URL required; rest are optional
+      var request = function(url, params, verb, data, cache) {
+        var deferred = $q.defer();
+        $http({
+          method: (verb) ? verb : 'GET',
+          url: url,
+          data: data,
+          params: params,
+          withCredentials: true,
+          cache: (cache)
+        }).success(function(response) {
+          deferred.resolve(response.value || response.data || response);
+        }).error(function(response) {
+          deferred.reject(response);
+        });
+
+        return deferred.promise;
+      };
+
+      // requestSettings: from $http documentation
+      // method: string
+      // url: string
+      // params: object
+      // data: string/object
+      // headers: object
+      // xsrfHeaderName: string
+      // xsrfCookieName: string
+      // transformRequest
+      // transformResponse
+      // cache: boolean or Cache
+      // timeout: number or Promise
+      // withCredentials: boolean
+      // responseType: string
+      var requestRaw = function(requestSettings) {
+        var deferred = $q.defer();
+        $http(requestSettings).success(function(data, status, headers, config) {
+          var response = {
+            data: data,
+            status: status,
+            headers: headers(),
+            config: config
+          };
+          deferred.resolve(response);
+        }).error(function(data, status, headers, config) {
+          var response = {
+            data: data,
+            status: status,
+            headers: headers,
+            config: config
+          };
+          deferred.reject(response);
+        });
+        return deferred.promise;
+      };
+
+      return {
+        request: request,
+        requestRaw: requestRaw
+      }
+    }
+  ]
+);
