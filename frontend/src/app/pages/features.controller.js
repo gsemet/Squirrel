7dca9e572eb6c4635f087ca484f139301ae00d56
Gaetan Semet <gaetan@xeberon.net>
2015-04-19 16:30:03 +0200

    use duScroll for smoothscrolling

--- frontend/src/app/pages/features.controller.js
+++ frontend/src/app/pages/features.controller.js
@@ -2,15 +2,41 @@
 
 angular.module("squirrel").controller("FeaturesCtrl",
 
-  ["$scope", "$location", "AuthenticationService", "$anchorScroll",
+  ["$scope", "$rootScope", "$location", "AuthenticationService", "$anchorScroll", "gettextCatalog",
+  "$document",
 
-    function($scope, $location, AuthenticationService, $anchorScroll) {
+    function($scope, $rootScope, $location, AuthenticationService, $anchorScroll, gettextCatalog,
+      $document) {
 
       $scope.scrollTo = function(id) {
-        $location.hash(id);
-        $anchorScroll();
+        /* $location.hash(id); */
+        /* $anchorScroll(); */
+        var offset = 130;
+        var duration = 400; //milliseconds
+        var targetElement = angular.element(document.getElementById(id));
+        // use duScroll for smooth scrolling
+        $document.scrollToElement(targetElement, offset, duration);
       };
 
+      $rootScope.$on('$duScrollChanged', function($event, scrollY) {
+        console.log('Scrolled to ', scrollY);
+      });
+
+      $scope.items = [
+        {
+          "key": "center-history",
+          "text": gettextCatalog.getString("Center of your Finance")
+        }, {
+          "key": "visualizations",
+          "text": gettextCatalog.getString("Data Visualization")
+        }, {
+          "key": "anywhere",
+          "text": gettextCatalog.getString("Anywhere, Anytime")
+        }, {
+          "key": "share",
+          "text": gettextCatalog.getString("Share your Portfolio")
+        }
+      ];
     }
   ]
 );
