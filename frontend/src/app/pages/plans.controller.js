92e89310a68eeb57c2e5d24155ee8ee2df274992
Gaetan Semet <gaetan@xeberon.net>
2015-04-27 23:31:52 +0200

    rework plans page
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- frontend/src/app/pages/plans.controller.js
+++ frontend/src/app/pages/plans.controller.js
@@ -2,12 +2,55 @@
 
 angular.module("squirrel").controller("PlansCtrl",
 
-  ["$scope", "$location", "AuthenticationService", "$document",
+  ["$scope", "$location", "AuthenticationService", "$document", "gettextCatalog",
 
-    function($scope, $location, AuthenticationService, $document) {
+    function($scope, $location, AuthenticationService, $document, gettextCatalog) {
 
       $document.scrollTo(0, 0);
 
+      /* Adapt odd_class and even_class to have nice display on the screen! */
+
+      $scope.features = [
+        {
+          "name": gettextCatalog.getString("Portfolio Control"),
+          "odd_class": "light",
+          "even_class": "",
+          "features": [
+            {
+              "description": gettextCatalog.getString('View and Edit all your Portfolios'),
+              "free": true,
+              "premium": true,
+            },
+            {
+              "description": gettextCatalog.getString("Weekly / Daily Portfolio Report by email"),
+              "free": true,
+              "premium": true,
+            },
+            {
+              "description": gettextCatalog.getString("Quaterly Portfolio Report by email"),
+              "free": true,
+              "premium": true,
+            }
+          ]
+        },
+        {
+          "name": gettextCatalog.getString("Measure"),
+          "odd_class": "",
+          "even_class": "light",
+          "features": [
+            {
+              "description": gettextCatalog.getString("Return, Gain, Alpha"),
+              "free": true,
+              "premium": true,
+            },
+            {
+              "description": gettextCatalog.getString("Share price"),
+              "free": true,
+              "premium": true,
+            }
+          ]
+        }
+      ]
     }
   ]
 );
