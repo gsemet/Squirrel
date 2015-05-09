d3ac43182c1f392c30b4d885e13fa80d6f308f73
Gaetan Semet <gaetan@xeberon.net>
2015-05-09 13:53:33 +0200

    Fix regression in proxy.js

--- frontend/gulp/proxy.js
+++ frontend/gulp/proxy.js
@@ -1,6 +1,6 @@
- /*jshint unused:false */
+/*jshint unused:false */
 
- /***************
+/***************
 
    This file allow to configure a proxy system plugged into BrowserSync
    in order to redirect backend requests while still serving and watching
@@ -11,71 +11,69 @@
    If you want to enable it, watch at the configuration options and finally
    change the `module.exports` at the end of the file
 
- ***************/
+***************/
 
- 'use strict';
+'use strict';
 
- var httpProxy = require('http-proxy');
- var chalk = require('chalk');
- var historyApiFallback = require('connect-history-api-fallback')
- var enableHtml5ModeFallback = true;
+var httpProxy = require('http-proxy');
+var chalk = require('chalk');
+var historyApiFallback = require('connect-history-api-fallback')
+var enableHtml5ModeFallback = true;
 
- /*
-  * Location of your backend server
-  */
- var proxyTarget = 'http://localhost:8080/';
+/*
+ * Location of your backend server
+ */
+var proxyTarget = 'http://localhost:8080/';
 
- var proxy = httpProxy.createProxyServer({
-   target: proxyTarget
- });
+var proxy = httpProxy.createProxyServer({
+  target: proxyTarget
+});
 
- proxy.on('error', function(error, req, res) {
-   res.writeHead(500, {
-     'Content-Type': 'text/plain'
-   });
+proxy.on('error', function(error, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
 
-   console.error(chalk.red('[Proxy]'), error);
- });
+  console.error(chalk.red('[Proxy]'), error);
+});
 
- /*
-  * The proxy middleware is an Express middleware added to BrowserSync to
-  * handle backend request and proxy them to your backend.
-  */
- function proxyMiddleware(req, res, next) {
-   /*
-    * This test is the switch of each request to determine if the request is
-    * for a static file to be handled by BrowserSync or a backend request to proxy.
-    *
-    * The existing test is a standard check on the files extensions but it may fail
-    * for your needs. If you can, you could also check on a context in the url which
-    * may be more reliable but can't be generic.
-    */
-   /* Default test:
-    *
-    *  /\.(html|css|js|png|jpg|jpeg|gif|ico|xml|rss|txt|eot|svg|ttf|woff|cur)(\?((r|v|rel|rev)=[\-\.\w]*)?)?$/
-    */
-   if (/^\/api\//.test(req.url)) {
-     // route all request to /api/* to backend running on port 3000
-     console.log("[serving w/ proxy] " + req.url);
-     proxy.web(req, res);
-   } else {
-     if (enableHtml5ModeFallback) {
-       console.log("[serving w/o proxy (html5mode fallback)] " + req.url);
-       return historyApiFallback({
-         verbose: false
-       })(req, res, next);
-     } else {
-       console.log("[serving w/o proxy] " + req.url);
-       next();
-     }
-   }
+/*
+ * The proxy middleware is an Express middleware added to BrowserSync to
+ * handle backend request and proxy them to your backend.
+ */
+function proxyMiddleware(req, res, next) {
+  /*
+   * This test is the switch of each request to determine if the request is
+   * for a static file to be handled by BrowserSync or a backend request to proxy.
+   *
+   * The existing test is a standard check on the files extensions but it may fail
+   * for your needs. If you can, you could also check on a context in the url which
+   * may be more reliable but can't be generic.
+   */
+  /* Default test:
+   *
+   *  /\.(html|css|js|png|jpg|jpeg|gif|ico|xml|rss|txt|eot|svg|ttf|woff|cur)(\?((r|v|rel|rev)=[\-\.\w]*)?)?$/
+   */
+  if (/^\/api\//.test(req.url)) {
+    // route all request to /api/* to backend running on port 8080
+    console.log("[serving w/ proxy] " + req.url);
+    proxy.web(req, res);
+  } else if (enableHtml5ModeFallback) {
+    console.log("[serving w/o proxy (html5mode fallback)] " + req.url);
+    return historyApiFallback({
+      verbose: false
+    })(req, res, next);
+  } else {
+    console.log("[serving w/o proxy] " + req.url);
+    next();
+  }
+}
 
-   /*
-    * This is where you activate or not your proxy.
-    *
-    * The first line activate it and the second one ignored it
-    */
+/*
+ * This is where you activate or not your proxy.
+ *
+ * The first line activate it and the second one ignored it
+ */
 
-   module.exports = [proxyMiddleware];
-   //module.exports = [];
- }
+module.exports = [proxyMiddleware];
+//module.exports = [];
