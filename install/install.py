a36f06653948ec71ea9834b74f4d5ca9440e3fab
Gaetan Semet <gaetan@xeberon.net>
2015-05-11 11:36:53 +0200

    install.py => main()
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- install/install.py
+++ install/install.py
@@ -25,8 +25,6 @@ isWindows = False
 if sys.platform.startswith('win32'):
     isWindows = True
 
-do_virtualenv = True
-
 # Injecting available targets from installer stage 2
 pkg = imp.load_source('install.stage2',
                       os.path.join(os.path.dirname(__file__), "install-stage2.py"))
@@ -134,132 +132,138 @@ def usage():
     sys.exit(0)
 
 
-if len(sys.argv) > 1:
-    args = sys.argv[:]
-    while args:
-        executable = args.pop(0)
-        subcmd = args.pop(0)
-        if subcmd in {"-h", "--help"}:
-            usage()
-        subcmd = aliases.get(subcmd, subcmd)
-        if subcmd not in allowed_cmd.keys():
-            print("Invalid command: {}".format(subcmd))
-            print("See usage with --help")
-            sys.exit(1)
-else:
-    print("No argument in the command line, using default target: {}".format(default_cmd))
-    subcmd = default_cmd
+def main():
+    do_virtualenv = True
 
-if sys.version_info < (2, 7):
-    raise Exception("must use python 2.7.x. Current version is: {}.".format(sys.version_info))
+    if len(sys.argv) > 1:
+        args = sys.argv[:]
+        while args:
+            # removing executable name
+            args.pop(0)
+            subcmd = args.pop(0)
+            if subcmd in {"-h", "--help"}:
+                usage()
+            subcmd = aliases.get(subcmd, subcmd)
+            if subcmd not in allowed_cmd.keys():
+                print("Invalid command: {}".format(subcmd))
+                print("See usage with --help")
+                sys.exit(1)
+    else:
+        print("No argument in the command line, using default target: {}".format(default_cmd))
+        subcmd = default_cmd
 
-if sys.version_info >= (3, 0):
-    raise Exception("must use python 2.7.x. Current version is: {}.".format(sys.version_info))
+    if sys.version_info < (2, 7):
+        raise Exception("must use python 2.7.x. Current version is: {}.".format(sys.version_info))
 
-if "novirtualenv" in subcmd:
-    do_virtualenv = False
+    if sys.version_info >= (3, 0):
+        raise Exception("must use python 2.7.x. Current version is: {}.".format(sys.version_info))
 
-if os.environ.get('VIRTUAL_ENV'):
-    printError("Beware, you already are inside the following virtual env: {}"
-               .format(os.environ.get('VIRTUAL_ENV')))
-    printError("Please leave it with 'deactivate' "
-               "and relaunch your command, unless you understand what is going on.")
+    if "novirtualenv" in subcmd:
+        do_virtualenv = False
 
-install_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "configs", "default.conf"))
-stage2_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "install-stage2.py"))
+    if os.environ.get('VIRTUAL_ENV'):
+        printError("Beware, you already are inside the following virtual env: {}"
+                   .format(os.environ.get('VIRTUAL_ENV')))
+        printError("Please leave it with 'deactivate' "
+                   "and relaunch your command, unless you understand what is going on.")
 
-with open(config_path) as f:
-    config = f.readlines()
+    install_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    # config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "configs", "default.conf"))
+    stage2_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "install-stage2.py"))
 
-workdir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "workdir"))
-requirements_txt = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                os.pardir,
-                                                "requirements.txt"))
-if isWindows:
-    activate = os.path.join(workdir_path, "Scripts", "activate.bat")
-    activate_info = activate
-    os_str = "Windows"
-else:
-    activate = os.path.join(workdir_path, "bin", "activate")
-    activate_info = "source {0}".format(activate)
-    os_str = "Posix"
+    # with open(config_path) as f:
+    #     config = f.readlines()
 
-printSeparator("=")
-printBoot("Squirrel Installer Stage 1")
-printBoot("Executing command: '{}'".format(subcmd))
-printBoot("Platform: {0}".format(sys.platform))
-printBoot("Environment: {0}".format(os_str))
-printBoot("Interpreter: {0} - Version: {1}".format(sys.executable, sys.version.split("\n")[0]))
-if do_virtualenv:
-    printBoot("Setting up virtualenv to start Installer Stage 2.")
-else:
-    m = "Beware !! 'novirtualenv' mode detected, do ** NOT ** setup a virtual env!"
-    printBoot("!" * len(m))
-    printBoot(m)
-    printBoot("*** Hope your production works inside a Docker !! *** ")
-    printBoot("!" * len(m))
-printBoot("You can activate this environment with the following command:")
-printBoot("    {0}".format(activate_info))
-printBoot("Installing in {0}".format(workdir_path))
-printBoot("Requirements: {0}".format(requirements_txt))
-
-
-if isWindows:
-    virtualenv = "virtualenv.exe"
-    python_exe = "python.exe"
-    launch_in_new_window = True
-
-    if not os.path.exists(os.path.join(workdir_path, "Scripts", "pip.exe")):
-        printBoot("Installing virtualenv in: {0}".format(workdir_path))
-        subprocess.check_call([virtualenv, "--system-site-packages", workdir_path])
-
-    # using launcher instead of activate.bat because we want to launch custom commands
-    launcher_bat = os.path.abspath(os.path.join(os.path.dirname(__file__), "launcher.bat"))
-
-    printBoot("Activating virtualenv in {0}".format(workdir_path))
-    subprocess.check_call([
-        "cmd", "/K",
-        launcher_bat, "new_window" if launch_in_new_window else "no_new_window",
-        workdir_path, stage2_path, install_path, workdir_path, subcmd])
-
-elif sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
-
-    if do_virtualenv:
-        if "VIRTUAL_ENV" in os.environ and not os.environ['VIRTUAL_ENV']:
-            printBoot("Note: Already in a virtualenv!")
-
+    workdir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "workdir"))
+    requirements_txt = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                    os.pardir,
+                                                    "requirements.txt"))
+    if isWindows:
+        activate = os.path.join(workdir_path, "Scripts", "activate.bat")
+        activate_info = activate
+        os_str = "Windows"
+    else:
         activate = os.path.join(workdir_path, "bin", "activate")
+        activate_info = "source {0}".format(activate)
+        os_str = "Posix"
 
-        if not os.path.exists(os.path.join(workdir_path, "bin", "pip")):
-            subprocess.check_call(['virtualenv', workdir_path])
+    printSeparator("=")
+    printBoot("Squirrel Installer Stage 1")
+    printBoot("Executing command: '{}'".format(subcmd))
+    printBoot("Platform: {0}".format(sys.platform))
+    printBoot("Environment: {0}".format(os_str))
+    printBoot("Interpreter: {0} - Version: {1}".format(sys.executable, sys.version.split("\n")[0]))
+    if do_virtualenv:
+        printBoot("Setting up virtualenv to start Installer Stage 2.")
+    else:
+        m = "Beware !! 'novirtualenv' mode detected, do ** NOT ** setup a virtual env!"
+        printBoot("!" * len(m))
+        printBoot(m)
+        printBoot("*** Hope your production works inside a Docker !! *** ")
+        printBoot("!" * len(m))
+    printBoot("You can activate this environment with the following command:")
+    printBoot("    {0}".format(activate_info))
+    printBoot("Installing in {0}".format(workdir_path))
+    printBoot("Requirements: {0}".format(requirements_txt))
 
-        if not os.path.exists(os.path.join(install_path, "activate")):
-            printBoot("Creating symblink activate")
-            os.symlink(os.path.join(workdir_path, "bin", "activate"), os.path.join(install_path,
-                                                                                   "activate"))
+    if isWindows:
+        virtualenv = "virtualenv.exe"
+        # python_exe = "python.exe"
+        launch_in_new_window = True
+
+        if not os.path.exists(os.path.join(workdir_path, "Scripts", "pip.exe")):
+            printBoot("Installing virtualenv in: {0}".format(workdir_path))
+            subprocess.check_call([virtualenv, "--system-site-packages", workdir_path])
+
+        # using launcher instead of activate.bat because we want to launch custom commands
+        launcher_bat = os.path.abspath(os.path.join(os.path.dirname(__file__), "launcher.bat"))
 
         printBoot("Activating virtualenv in {0}".format(workdir_path))
-        # subprocess.check_call([python_exe, stage2_path, activate, install_path])
         subprocess.check_call([
-            'bash',
-            '-c',
-            'source {activate} && python {stage2} {install_path} {workdir_path} {subcmd}'
-            .format(activate=activate,
-                    stage2=stage2_path,
-                    install_path=install_path,
-                    workdir_path=workdir_path,
-                    subcmd=subcmd)])
-    else:
-        printBoot("Starting stage 2 directly without installing a virtualenv")
-        # subprocess.check_call([python_exe, stage2_path, activate, install_path])
-        subprocess.check_call([
-            'python',
-            stage2_path,
-            install_path,
-            workdir_path,
-            subcmd,
-        ])
+            "cmd", "/K",
+            launcher_bat, "new_window" if launch_in_new_window else "no_new_window",
+            workdir_path, stage2_path, install_path, workdir_path, subcmd])
 
-else:
-    raise Exception("Unsupported environment: {0}".format(sys.platform))
+    elif sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
+
+        if do_virtualenv:
+            if "VIRTUAL_ENV" in os.environ and not os.environ['VIRTUAL_ENV']:
+                printBoot("Note: Already in a virtualenv!")
+
+            activate = os.path.join(workdir_path, "bin", "activate")
+
+            if not os.path.exists(os.path.join(workdir_path, "bin", "pip")):
+                subprocess.check_call(['virtualenv', workdir_path])
+
+            if not os.path.exists(os.path.join(install_path, "activate")):
+                printBoot("Creating symblink activate")
+                os.symlink(os.path.join(workdir_path, "bin", "activate"), os.path.join(install_path,
+                                                                                       "activate"))
+
+            printBoot("Activating virtualenv in {0}".format(workdir_path))
+            # subprocess.check_call([python_exe, stage2_path, activate, install_path])
+            subprocess.check_call([
+                'bash',
+                '-c',
+                'source {activate} && python {stage2} {install_path} {workdir_path} {subcmd}'
+                .format(activate=activate,
+                        stage2=stage2_path,
+                        install_path=install_path,
+                        workdir_path=workdir_path,
+                        subcmd=subcmd)])
+        else:
+            printBoot("Starting stage 2 directly without installing a virtualenv")
+            # subprocess.check_call([python_exe, stage2_path, activate, install_path])
+            subprocess.check_call([
+                'python',
+                stage2_path,
+                install_path,
+                workdir_path,
+                subcmd,
+            ])
+
+    else:
+        raise Exception("Unsupported environment: {0}".format(sys.platform))
+
+if __name__ == "__main__":
+    main()
