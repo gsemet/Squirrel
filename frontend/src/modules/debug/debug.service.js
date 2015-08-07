318c4685cdbd00f7c82e4bb668fda50baff50f69
Gaetan Semet <gaetan@xeberon.net>
2015-08-07 23:26:34 +0200

    prefix in debug module
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- frontend/src/modules/debug/debug.service.js
+++ frontend/src/modules/debug/debug.service.js
@@ -16,30 +16,30 @@ angular.module('squirrel').factory('debug',
 
       debug_service.log = function(module, text) {
         if (debug_service.enabled) {
-          $log.log("[" + module + "] " + text);
+          $log.log("LOG: [" + module + "] " + text);
         }
       };
 
       debug_service.warn = function(module, text) {
-        $log.warn("%c [" + module + "] " + text,
+        $log.warn("WARN: %c [" + module + "] " + text,
           'background: #CF8902; color: #000000');
       };
 
       debug_service.info = function(module, text) {
         if (debug_service.enabled) {
-          $log.info("%c [" + module + "] " + text,
+          $log.info("INFO: %c [" + module + "] " + text,
             'color: #0006FF');
         }
       };
 
       debug_service.error = function(module, text) {
-        $log.error("%c [" + module + "] " + text,
+        $log.error("ERROR: %c [" + module + "] " + text,
           'background: #E20000; color: #FFFFFF');
       };
 
       debug_service.debug = function(module, text) {
         if (debug_service.enabled) {
-          $log.debug("%c [" + module + "] " + text, 'background: #FFFFFF; color: #444444');
+          $log.debug("DEBUG: %c [" + module + "] " + text, 'background: #FFFFFF; color: #444444');
         }
       };
 
@@ -48,7 +48,7 @@ angular.module('squirrel').factory('debug',
           if (!name) {
             name = "Object"
           }
-          $log.log("%c [" + module + "] " + name + ": " + JSON.stringify(obj),
+          $log.log("LOG: %c [" + module + "] " + name + ": " + JSON.stringify(obj),
             'background: #DFDFDF; color: #000000#000000');
         }
       };
