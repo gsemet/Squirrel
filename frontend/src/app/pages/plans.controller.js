3b19c137e623b487f9bf8c69f82452708f55a3a3
Gaetan Semet <gaetan@xeberon.net>
2015-08-15 15:44:53 +0200

    fix plan page
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- frontend/src/app/pages/plans.controller.js
+++ frontend/src/app/pages/plans.controller.js
@@ -20,40 +20,37 @@ angular.module("squirrel").controller("PlansCtrl",
               "description": gettextCatalog.getString('View and Edit one Portfolio'),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString('Number of Portfolios'),
               "free": 2,
               "premium": "unlimited",
-            },
-            {
-              "description": gettextCatalog.getString('View and Edit all your Portfolios'),
+            }, {
+              "description": gettextCatalog.getString('Group Your Portfolios into Classes'),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString('Track Every Assets Type'),
               "free": true,
               "premium": true,
-            },
-            {
-              "description": gettextCatalog.getString('Automatic Stocks and Mutual Funds Updates'),
+            }, {
+              "description": gettextCatalog.getString('Automatic Stocks, Mutual Funds, ETF Updates'),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
+              "description": gettextCatalog.getString("All Exchange Places"),
+              "free": true,
+              "premium": true,
+            }, {
               "description": gettextCatalog.getString("Weekly / Daily Portfolio Report by email"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Personal History Timeline"),
               "free": true,
               "premium": true,
             }
           ]
-        },
-        {
+        }, {
           "name": gettextCatalog.getString("Data Visualisation"),
           "odd_class": "light",
           "even_class": "",
@@ -62,35 +59,33 @@ angular.module("squirrel").controller("PlansCtrl",
               "description": gettextCatalog.getString("Return, Gain, Alpha"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
+              "description": gettextCatalog.getString("Tax-aware Return"),
+              "free": true,
+              "premium": true,
+            }, {
               "description": gettextCatalog.getString("Share Price History"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Geography Exposure"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Small/Big Caps Exposure"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Assets Classes Exposure"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Compare With N Benchmarks (Stocks, Funds, ETF)"),
               "free": true,
               "premium": true,
             }
           ]
-        },
-        {
+        }, {
           "name": gettextCatalog.getString("Stocks Screener"),
           "odd_class": "light",
           "even_class": "",
@@ -99,25 +94,21 @@ angular.module("squirrel").controller("PlansCtrl",
               "description": gettextCatalog.getString("Dynamic Stocks Comparison"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Smart Stock Search History"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Save Screener Settings"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Screener Staging Area"),
               "free": true,
               "premium": true,
             }
           ]
-        },
-        {
+        }, {
           "name": gettextCatalog.getString("Reports and Reminder"),
           "odd_class": "light",
           "even_class": "",
@@ -126,30 +117,25 @@ angular.module("squirrel").controller("PlansCtrl",
               "description": gettextCatalog.getString("User defined Periodic Reminder for Manual Updates"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Weekly / Daily Portfolio Report by email"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Quaterly Portfolio Report by email"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Annual Portfolio Report edition"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Edit your annual Portfolio Report"),
               "free": true,
               "premium": true,
             }
           ]
-        },
-        {
+        }, {
           "name": gettextCatalog.getString("Tax Statement"),
           "odd_class": "light",
           "even_class": "",
@@ -158,8 +144,7 @@ angular.module("squirrel").controller("PlansCtrl",
               "description": gettextCatalog.getString("Easy Gains/Loose Tracking for your Tax Statement"),
               "free": true,
               "premium": true,
-            },
-            {
+            }, {
               "description": gettextCatalog.getString("Speculative Account Monthly Report Storage"),
               "free": true,
               "premium": true,
