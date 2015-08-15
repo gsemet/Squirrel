a5ad8f664c2d335deaf141141a0b45e6b14f60c1
Gaetan Semet <gaetan@xeberon.net>
2015-08-15 16:17:47 +0200

    rework portfolio menu

--- frontend/src/app/portfolios/_portfolios.controller.js
+++ frontend/src/app/portfolios/_portfolios.controller.js
@@ -18,124 +18,15 @@ angular.module("squirrel").controller("PortfoliosCtrl",
         $location.search("p", "summary");
       }
 
-      var features = environment.getFeatures();
-
       $scope.portfolioIndexInSidebar = 0;
-      $scope.menuItems = [
-        {
-          search: {
-            'p': 'summary'
-          },
-          name: gettextCatalog.getString('Summary'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/summary.template.html',
-          state: features.portfolio.summary,
-        }, {
-          search: {
-            'p': 'securities'
-          },
-          name: gettextCatalog.getString('Securities'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/securities.template.html',
-          state: features.portfolio.securities,
-        }, {
-          search: {
-            'p': 'cash'
-          },
-          name: gettextCatalog.getString('Cash'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/cash.template.html',
-          state: features.portfolio.cash,
-        }, {
-          search: {
-            'p': 'allocations'
-          },
-          name: gettextCatalog.getString('Allocations'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/allocations.template.html',
-          state: features.portfolio.allocations,
-        }, {
-          search: {
-            'p': 'watchlist'
-          },
-          name: gettextCatalog.getString('Watchlist'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/watchlist.template.html',
-          state: features.portfolio.watchlist,
-        }, {
-          search: {
-            'p': 'covers'
-          },
-          name: gettextCatalog.getString('Covers'),
-          icon: 'glyphicon glyphicon-book',
-          templateUrl: 'app/portfolios/covers.template.html',
-          state: features.portfolio.covers,
-        }, {
-          search: {
-            'p': 'timeline', // == move history
-          },
-          name: gettextCatalog.getString('Timeline'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/timeline.template.html',
-          state: features.portfolio.timeline,
-        }, {
-          search: {
-            'p': 'status_report'
-          },
-          name: gettextCatalog.getString('Status Report'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/status_report.template.html',
-          state: features.portfolio.reports,
-        }, {
-          search: {
-            'p': 'reporting'
-          },
-          name: gettextCatalog.getString('Reporting'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/reporting.template.html',
-          state: features.portfolio.reporting,
-        }, {
-          search: {
-            'p': 'annual_reports'
-          },
-          name: gettextCatalog.getString('Annual Reports'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/annual_reports.template.html',
-          state: features.portfolio.reports,
-        }, {
-          search: {
-            'p': 'taxation'
-          },
-          name: gettextCatalog.getString('Taxation'),
-          icon: 'glyphicon glyphicon-dashboard',
-          templateUrl: 'app/portfolios/taxation.template.html',
-          state: features.portfolio.taxation,
-        }
-      ];
+      $scope.menuItems = [];
 
-      $scope.is_logged = AuthenticationService.isAuthenticated();
-      debug.log("PortfoliosCtrl", "my portofolio is_logged = " + JSON.stringify($scope.is_logged));
-
-      $rootScope.$on(AUTH_EVENTS.loginSuccess, function(event, userName) {
-        $scope.is_logged = AuthenticationService.isAuthenticated();
-      });
-
-      $rootScope.$on(AUTH_EVENTS.logoutSuccess, function(event) {
-        $scope.is_logged = AuthenticationService.isAuthenticated();
-      });
-
-      $rootScope.$on(AUTH_EVENTS.loginFailed, function(event, error) {
-        $scope.is_logged = AuthenticationService.isAuthenticated();
-      });
-
-      var basePortfolios = Restangular.all("api/portfolios/p");
-
-      $scope.portfolios = [];
       $scope.refresh = function() {
+        debug.debug("PortfoliosCtrl", "refreshing...");
         $scope.portfolios = [];
         basePortfolios.getList().then(function(data) {
           debug.dump("PortfoliosCtrl", data, "received portfolios data for sidebar");
-          $scope.menuItems[$scope.portfolioIndexInSidebar]['children'] = []
+          $scope.menuItems[$scope.portfolioIndexInSidebar]['children'] = [];
           _.forEach(data, function(row) {
             $scope.portfolios.push(row);
             $scope.menuItems[$scope.portfolioIndexInSidebar]['children'].push({
@@ -151,7 +42,122 @@ angular.module("squirrel").controller("PortfoliosCtrl",
           sidebar.refresh();
         });
       };
-      $timeout($scope.refresh, 500);
+
+      $rootScope.$on(environment.ENVIRONMENT_FOUND, function(event) {
+        debug.debug("PortfoliosCtrl", "on environment found event");
+        var features = environment.getFeatures();
+        $scope.menuItems = [
+          {
+            search: {
+              'p': 'summary'
+            },
+            name: gettextCatalog.getString('Summary'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/summary.template.html',
+            state: features.portfolio.summary,
+          }, {
+            search: {
+              'p': 'securities'
+            },
+            name: gettextCatalog.getString('Securities'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/securities.template.html',
+            state: features.portfolio.securities,
+          }, {
+            search: {
+              'p': 'cash'
+            },
+            name: gettextCatalog.getString('Cash'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/cash.template.html',
+            state: features.portfolio.cash,
+          }, {
+            search: {
+              'p': 'allocations'
+            },
+            name: gettextCatalog.getString('Allocations'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/allocations.template.html',
+            state: features.portfolio.allocations,
+          }, {
+            search: {
+              'p': 'watchlist'
+            },
+            name: gettextCatalog.getString('Watchlist'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/watchlist.template.html',
+            state: features.portfolio.watchlist,
+          }, {
+            search: {
+              'p': 'covers'
+            },
+            name: gettextCatalog.getString('Covers'),
+            icon: 'glyphicon glyphicon-book',
+            templateUrl: 'app/portfolios/covers.template.html',
+            state: features.portfolio.covers,
+          }, {
+            search: {
+              'p': 'timeline', // == move history
+            },
+            name: gettextCatalog.getString('Timeline'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/timeline.template.html',
+            state: features.portfolio.timeline,
+          }, {
+            search: {
+              'p': 'status_report'
+            },
+            name: gettextCatalog.getString('Status Report'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/status_report.template.html',
+            state: features.portfolio.reports,
+          }, {
+            search: {
+              'p': 'reporting'
+            },
+            name: gettextCatalog.getString('Reporting'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/reporting.template.html',
+            state: features.portfolio.reporting,
+          }, {
+            search: {
+              'p': 'annual_reports'
+            },
+            name: gettextCatalog.getString('Annual Reports'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/annual_reports.template.html',
+            state: features.portfolio.reports,
+          }, {
+            search: {
+              'p': 'taxation'
+            },
+            name: gettextCatalog.getString('Taxation'),
+            icon: 'glyphicon glyphicon-dashboard',
+            templateUrl: 'app/portfolios/taxation.template.html',
+            state: features.portfolio.taxation,
+          }
+        ];
+        $scope.refresh();
+      });
+
+      $scope.is_logged = AuthenticationService.isAuthenticated();
+      debug.debug("PortfoliosCtrl", "my portofolio is_logged = " + JSON.stringify($scope.is_logged));
+
+      $rootScope.$on(AUTH_EVENTS.loginSuccess, function(event, userName) {
+        debug.debug("PortfoliosCtrl", "login successful event");
+        $scope.is_logged = AuthenticationService.isAuthenticated();
+      });
+
+      $rootScope.$on(AUTH_EVENTS.logoutSuccess, function(event) {
+        $scope.is_logged = AuthenticationService.isAuthenticated();
+      });
+
+      $rootScope.$on(AUTH_EVENTS.loginFailed, function(event, error) {
+        $scope.is_logged = AuthenticationService.isAuthenticated();
+      });
+
+      var basePortfolios = Restangular.all("api/portfolios/p");
+      $scope.portfolios = [];
 
       $scope.sidebar_class = "active";
       $scope.toggleSidebar = function() {
@@ -208,6 +214,6 @@ angular.module("squirrel").controller("PortfoliosCtrl",
         debug.dump("$controller = " + c);
         return c;
       };*/
-        }
-        ]
+    }
+  ]
 );
